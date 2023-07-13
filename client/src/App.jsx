c27745506f64e8db94d2e02b876a1c3a574cef3f
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-07-13 11:25:53 +0530

    Updated Profile Display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,6 @@ import ForgetPswd from '../Components/Register/ForgetPswd'
 import MainDashboard from '../Components/Dashboard/MainDashboard'
 import MainContent from '../Components/Dashboard/MainContent'
 import PersonalDasboard from '../Pages/Project/PersonalDashboard'
-<<<<<<< HEAD
-import CreateProject1 from '../Pages/Project/CreateProject1'
-import CreateProject2 from '../Pages/Project/CreateProject2'
-const App = () => {
-  return (
-    < >
-    {/* <Login />
-    <Register /> */}
-    {/* <ForgetPswd/> */}
-    {/* <MainDashboard /> */}
-    {/* <PersonalDasboard></PersonalDasboard> */}
-    {/* <CreateProject1></CreateProject1> */}
-    <CreateProject2></CreateProject2>
-=======
 import Project from "../Pages/Project/ProjectDisplay"
 const App = () => {
   return (
@@ -29,7 +15,6 @@ const App = () => {
     <PersonalDasboard></PersonalDasboard>
     <MainDashboard />
     <Project />
->>>>>>> 6ecf051129a048b6f17879ac705be8641958e995
     </>
   )
 }
