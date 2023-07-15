9f9a7a52d8604ca4a681de35400d90589831764f
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2023-07-15 14:37:30 +0530

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,32 +5,27 @@ import ForgetPswd from '../Components/Register/ForgetPswd'
 import MainDashboard from '../Components/Dashboard/MainDashboard'
 import MainContent from '../Components/Dashboard/MainContent'
 import PersonalDasboard from '../Pages/Project/PersonalDashboard'
-<<<<<<< HEAD
 import CreateProject1 from '../Pages/Project/CreateProject1'
 import CreateProject2 from '../Pages/Project/CreateProject2'
+import ProjectPage from '../Pages/Project/ProjectPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
-    < >
-    {/* <Login />
-    <Register /> */}
-    {/* <ForgetPswd/> */}
-    {/* <MainDashboard /> */}
-    {/* <PersonalDasboard></PersonalDasboard> */}
-    {/* <CreateProject1></CreateProject1> */}
-    <CreateProject2></CreateProject2>
-=======
-import Project from "../Pages/Project/ProjectDisplay"
-const App = () => {
-  return (
-    < >
-    <Login />
-    <Register />
-    <ForgetPswd/>
-    <PersonalDasboard></PersonalDasboard>
-    <MainDashboard />
-    <Project />
->>>>>>> 6ecf051129a048b6f17879ac705be8641958e995
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/api/v1/login/:userid" element={<Login />}></Route> 
+    <Route path="/api/v1/register" element={<Register />}></Route> 
+    <Route path="/api/v1/password/forget" element={<ForgetPswd />}></Route> 
+    <Route path="/api/v1/projectdashboard/:userid" element={<PersonalDasboard />}></Route> 
+    <Route path="/api/v1/projectdashboard/:userid/projects/:projectid/tasks" element={<MainDashboard />}></Route> 
+    <Route path="/api/v1/createproject/:projectid" element={<CreateProject1 />}></Route> 
+    <Route path="/api/v1/users/:userid/projectpage/:projectid" element={<ProjectPage />}></Route> 
+    {/* <CreateProject2></CreateProject2> */}
+    </Routes>
+    </BrowserRouter>
     </>
+    
   )
 }
 
